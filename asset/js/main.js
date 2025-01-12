af5fde657faf8136f52fd0bea2c9baae64e5af01
arshina896 <arshina312@gmail.com>
2025-01-12 20:37:22 +0530

    last commit

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,12 +1,12 @@
-setTimeout(function() {
+setTimeout(function () {
 
-    AOS.init({
-        duration: 800,
-        easing: 'ease',
-        once: false
-    });
-   
-   }, 800 );
+  AOS.init({
+    duration: 800,
+    easing: 'ease',
+    once: false
+  });
+
+}, 800);
 let nextDom = document.getElementById('next');
 let prevDom = document.getElementById('prev');
 
@@ -20,71 +20,86 @@ thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
 let timeRunning = 3000;
 let timeAutoNext = 7000;
 
-nextDom.onclick = function(){
-    showSlider('next');    
+nextDom.onclick = function () {
+  showSlider('next');
 }
 
-prevDom.onclick = function(){
-    showSlider('prev');    
+prevDom.onclick = function () {
+  showSlider('prev');
 }
 let runTimeOut;
 let runNextAuto = setTimeout(() => {
-    next.click();
+  next.click();
 }, timeAutoNext)
-function showSlider(type){
-    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
-    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
-    
-    if(type === 'next'){
-        SliderDom.appendChild(SliderItemsDom[0]);
-        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-        carouselDom.classList.add('next');
-    }else{
-        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
-        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
-        carouselDom.classList.add('prev');
-    }
-    clearTimeout(runTimeOut);
-    runTimeOut = setTimeout(() => {
-        carouselDom.classList.remove('next');
-        carouselDom.classList.remove('prev');
-    }, timeRunning);
+function showSlider(type) {
+  let SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+  let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
 
-    clearTimeout(runNextAuto);
-    runNextAuto = setTimeout(() => {
-        next.click();
-    }, timeAutoNext)
-}
+  if (type === 'next') {
+    SliderDom.appendChild(SliderItemsDom[0]);
+    thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+    carouselDom.classList.add('next');
+  } else {
+    SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+    thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+    carouselDom.classList.add('prev');
+  }
+  clearTimeout(runTimeOut);
+  runTimeOut = setTimeout(() => {
+    carouselDom.classList.remove('next');
+    carouselDom.classList.remove('prev');
+  }, timeRunning);
 
-
-
-// 
+  clearTimeout(runNextAuto);
+  runNextAuto = setTimeout(() => {
+    next.click();
+  }, timeAutoNext)
+};
+// navbar
 document.addEventListener('DOMContentLoaded', function () {
-    const navbar = document.querySelector('.js-sticky-nav'); 
-    const scrollThreshold = 50; 
-  
-    window.addEventListener('scroll', function () {
-      if (window.scrollY > scrollThreshold) {
-        navbar.classList.add('scrolled'); 
-      } else {
-        navbar.classList.remove('scrolled'); 
-      }
-    });
+  const navbar = document.querySelector('.js-sticky-nav');
+  const scrollThreshold = 50;
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > scrollThreshold) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
   });
-
-
-
-
-  document.addEventListener('scroll', () => {
-    const boxes = document.querySelectorAll('.box');
-    const triggerHeight = window.innerHeight * 0.8; 
-  
-    boxes.forEach(box => {
-      const boxTop = box.getBoundingClientRect().top;
-  
-      if (boxTop < triggerHeight) {
-        box.classList.add('visible'); 
-      }
-    });
-  });
-  
+});
+// img-1
+document.addEventListener('scroll', () => {
+  const image = document.getElementById('img-view');
+  const triggerPosition = 300;
+  const scrollTop = window.scrollY;
+  if (scrollTop > triggerPosition) {
+    image.style.width = '50%';
+  }
+});
+// img-2
+document.addEventListener('scroll', () => {
+  const image2 = document.getElementById('img2-view');
+  const triggerPosition = 300;
+  const scrollTop = window.scrollY;
+  if (scrollTop > triggerPosition) {
+    image2.style.width = '60%';
+  }
+});
+// img-3
+document.addEventListener('scroll', () => {
+  const image3 = document.getElementById('room2a');
+  const triggerPosition = 850;
+  const scrollTop = window.scrollY;
+  if (scrollTop > triggerPosition) {
+    image3.style.width = '95%';
+  }
+});
+// img-4
+document.addEventListener('scroll', () => {
+  const image4 = document.getElementById('room2b');
+  const triggerPosition = 850;
+  const scrollTop = window.scrollY;
+  if (scrollTop > triggerPosition) {
+    image4.style.width = '60%';
+  }
+});
